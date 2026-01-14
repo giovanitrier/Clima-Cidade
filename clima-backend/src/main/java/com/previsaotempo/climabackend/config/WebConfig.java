50f0bd1dca51a28dce182827ccd3d7f8eff08da5
Giovani Trierweiler <giovanitrier@gmail.com>
2026-01-14 16:25:33 -0300

    Configuração do CORS para Produção

--- clima-backend/src/main/java/com/previsaotempo/climabackend/config/WebConfig.java
+++ clima-backend/src/main/java/com/previsaotempo/climabackend/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://front-end-production-bdad.up.railway.app")
                         .allowedMethods("*");
             }
         };
