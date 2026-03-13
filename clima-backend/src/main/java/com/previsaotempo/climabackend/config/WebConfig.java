61a5a8b41fc2389c160b2ac27c5df09cc99b0abc
Giovani Trierweiler <giovanitrier@gmail.com>
2026-03-13 10:21:21 -0300

    Atualizacao URL BACKEND

--- clima-backend/src/main/java/com/previsaotempo/climabackend/config/WebConfig.java
+++ clima-backend/src/main/java/com/previsaotempo/climabackend/config/WebConfig.java
@@ -13,8 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedOrigins("https://front-end-production-bdad.up.railway.app")
+                        .allowedOrigins("http://localhost:4200,https://clima-cidade.onrender.com")
                         .allowedMethods("*");
             }
         };
